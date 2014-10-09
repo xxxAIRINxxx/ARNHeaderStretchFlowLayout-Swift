3a9355fd70e804142433022187f206da9696b82c
xxxAIRINxxx <xl1138@gmail.com>
2014-10-09 15:34:35 +0900

    replace files

--- Pod/Classes/ARNHeaderStretchFlowLayout.swift
+++ Pod/Classes/ARNHeaderStretchFlowLayout.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ARNHeaderStretchFlowLayout: UICollectionViewFlowLayout {
     
-    override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
+    override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool
+    {
         return true
     }
     
-    override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]? {
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]?
+    {
         var offset = self.collectionView!.contentOffset
         var minY = 0 - self.collectionView!.contentInset.top
         
@@ -22,48 +24,18 @@ class ARNHeaderStretchFlowLayout: UICollectionViewFlowLayout {
         if offset.y < minY {
             var headerSize = self.headerReferenceSize
             var deltaY = fabsf(CFloat(offset.y) - CFloat(minY))
-            
             for attrs in attributes {
-                if attrs.representedElementKind == UICollectionElementKindSectionHeader &&
-                    attrs.indexPath.section == 0 {
+                var kind:String? = attrs.representedElementKind
+                if kind == UICollectionElementKindSectionHeader && attrs.indexPath.section == 0 {
                         var headerRect = attrs.frame
-                        headerRect.size.height = max(minY, CGFloat(headerSize.height + deltaY))
-                        headerRect.origin.y = headerRect.origin.y - deltaY
-                        
+                        headerRect.size.height = max(minY, CGFloat(headerSize.height) + CGFloat(deltaY))
+                        headerRect.origin.y = headerRect.origin.y - CGFloat(deltaY)
+                        attrs.frame = headerRect
+                        break
                 }
             }
         }
         
-        return attributes;
+        return attributes
     }
 }
-
-//- (UICollectionViewScrollDirection)scrollDirection
-//    {
-//        return UICollectionViewScrollDirectionVertical;
-//    }
-//    
-//    - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
-//{
-//    CGPoint offset = [[self collectionView] contentOffset];
-//    CGFloat minY   = 0 - [[self collectionView] contentInset].top;
-//    
-//    NSArray *attributes = [super layoutAttributesForElementsInRect:rect];
-//    
-//    if (offset.y < minY) {
-//        CGSize  headerSize = [self headerReferenceSize];
-//        CGFloat deltaY     = fabsf(offset.y - minY);
-//        
-//        for (UICollectionViewLayoutAttributes *attrs in attributes) {
-//            if ([attrs representedElementKind] == UICollectionElementKindSectionHeader &&
-//                attrs.indexPath.section == 0) {
-//                    CGRect headerRect = [attrs frame];
-//                    headerRect.size.height = MAX(minY, headerSize.height + deltaY);
-//                    headerRect.origin.y    = headerRect.origin.y - deltaY;
-//                    [attrs setFrame:headerRect];
-//                    break;
-//            }
-//        }
-//    }
-//    return attributes;
-//}
